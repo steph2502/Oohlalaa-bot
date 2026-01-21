55a5af85795d9b3f7c95a110eeb192541d7a3d12
Stephanie✨ <127240541+steph2502@users.noreply.github.com>
2026-01-21 14:32:56 +0100

    Update UserRoutes.js

--- api/src/routes/UserRoutes.js
+++ api/src/routes/UserRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { telegramAuth } = require("../controllers/userController");
+const { telegramAuth } = require("../controllers/UserController");
 
 router.post("/telegram-auth", telegramAuth);
 
